be2ad0af8e72dede8e95e725d064f7922e1b6ef4
mhdianrush <mhdianrush@gmail.com>
2023-07-10 20:41:28 +0700

    Developed Added isunique for validate tag for Checking the Similar Email User Input With Email in DB

--- entities/user.go
+++ entities/user.go
@@ -3,7 +3,7 @@ package entities
 type User struct {
 	Id              int64  `validate:"required" json:"id"`
 	FullName        string `validate:"required" label:"Full Name" json:"full_name"`
-	Email           string `validate:"required,email" json:"email"`
+	Email           string `validate:"required,email,isunique=users-email" json:"email"`
 	Username        string `validate:"required,gte=3" json:"username"`
 	Password        string `validate:"required,gte=6" json:"password"`
 	ConfirmPassword string `validate:"required,eqfield=Password" label:"Confirm Password" json:"confirm_password"`
