1c6d494578becd84e14730896f7cc84ee2a927b4
mhdianrush <mhdianrush@gmail.com>
2023-07-10 10:36:25 +0700

    Added Struct Field ConfirmPassword of Struct User

--- entities/user.go
+++ entities/user.go
@@ -1,11 +1,10 @@
 package entities
 
 type User struct {
-	Id       int64  `json:"id"`
-	FullName string `json:"full_name"`
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Id              int64  `json:"id"`
+	FullName        string `json:"full_name"`
+	Email           string `json:"email"`
+	Username        string `json:"username"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
 }
-
-
