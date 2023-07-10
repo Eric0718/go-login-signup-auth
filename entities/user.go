b5dd3ad2b27b1e7d38dd5b5005c0d2a9b2708438
mhdianrush <mhdianrush@gmail.com>
2023-07-10 21:35:48 +0700

    Added isunique in validation Tag on Username Field on User Struct to Check the Similarity of Username User Input With Username in db

--- entities/user.go
+++ entities/user.go
@@ -4,7 +4,7 @@ type User struct {
 	Id              int64  `validate:"required" json:"id"`
 	FullName        string `validate:"required" label:"Full Name" json:"full_name"`
 	Email           string `validate:"required,email,isunique=users-email" json:"email"`
-	Username        string `validate:"required,gte=3" json:"username"`
+	Username        string `validate:"required,gte=3,isunique=users-username" json:"username"`
 	Password        string `validate:"required,gte=6" json:"password"`
 	ConfirmPassword string `validate:"required,eqfield=Password" label:"Confirm Password" json:"confirm_password"`
 }
