73445d0502c18e47d04db837533ec1ea8e9d7868
mhdianrush <mhdianrush@gmail.com>
2023-07-31 10:31:56 +0700

    Developed for Using godotenv Package to Read .env File that Used in Server Addr

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/mhdianrush/go-login-signup-auth/config"
 	"github.com/mhdianrush/go-login-signup-auth/controllers"
 	"github.com/sirupsen/logrus"
@@ -14,12 +15,12 @@ import (
 func main() {
 	config.ConnectDB()
 
-	r := mux.NewRouter()
+	routes := mux.NewRouter()
 
-	r.HandleFunc("/", controllers.Index)
-	r.HandleFunc("/login", controllers.Login)
-	r.HandleFunc("/logout", controllers.Logout)
-	r.HandleFunc("/register", controllers.Register)
+	routes.HandleFunc("/", controllers.Index)
+	routes.HandleFunc("/login", controllers.Login)
+	routes.HandleFunc("/logout", controllers.Logout)
+	routes.HandleFunc("/register", controllers.Register)
 
 	logger := logrus.New()
 
@@ -27,13 +28,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	logger.SetOutput(file)
 
-	logger.Println("Server Running on Port 8080")
-
-	err = http.ListenAndServe(":8080", r)
-	if err != nil {
-		panic(err)
+	if err := godotenv.Load(); err != nil {
+		logger.Printf("failed load env file %s", err.Error())
 	}
+
+	server := http.Server{
+		Addr:    ":" + os.Getenv("SERVER_PORT"),
+		Handler: routes,
+	}
+	if err = server.ListenAndServe(); err != nil {
+		logger.Printf("failed connect to server %s", err.Error())
+	}
+	
+	logger.Printf("server running on port %s", os.Getenv("SERVER_PORT"))
 }
